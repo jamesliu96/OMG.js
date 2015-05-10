fff590ab7f27a0da3f31e965615748d6eb3ff06b
James Liu <jamesleu96@gmail.com>
2015-05-10 15:45:35 +0800

    String:safe

--- omg.js
+++ omg.js
@@ -78,6 +78,13 @@
     __extend(String.prototype, {
         reverse: function() {
             return this.split('').reverse().join('');
+        },
+        safe: function(a) {
+            return this.replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#039;");
         }
     });
 
